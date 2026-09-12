6704c3a44275f676b028d186f20995b6b37087c0
jung27 <junghhh4@gmail.com>
2026-09-12 12:27:19 +0900

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <div className="text-9xl text-center m-10 mt-40">어떡하집?</div>
+      <button className="btn btn-primary text-2xl mx-auto block px-10">
+        START
+      </button>
+    </div>
+  );
+}
+
+export default App;
